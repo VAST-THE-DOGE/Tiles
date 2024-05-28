5ec84646de50b37d3830eef03fb79774522150db
William Herbert <145150405+VAST-THE-DOGE@users.noreply.github.com>
2024-05-27 20:00:03 -0500

    Update MapGenerator.cs
    now the map generator creates beaches, so the land is generated fully. The only missing piece is additional tiles such as forests and mountains.

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -17,14 +17,14 @@ class MapGenerator {
         int mapSize = map.Length * map[0].Length;
 
         //create starting points:
-        int[] xPoints = new int[(int) Math.Ceiling((double) mapSize/100)];
+        int[] xPoints = new int[(int) Math.Ceiling(Math.Sqrt(mapSize/50))];
         int[] yPoints = new int[xPoints.Length];
         int[] PointHeight = new int[xPoints.Length];
         for (int i = 0; i < xPoints.Length; i++)
         {
             xPoints[i] = rnd.Next(0, map[0].Length);
             yPoints[i] = rnd.Next(0, map.Length);
-            PointHeight[i] = rnd.Next(1, 1 + (int) Math.Ceiling(Math.Sqrt(mapSize/150)));
+            PointHeight[i] = rnd.Next(1, Math.Clamp(1 + (int) Math.Ceiling(Math.Sqrt(mapSize/150)), 1, 4));
         }
 
         //set map height at points:
@@ -42,6 +42,7 @@ class MapGenerator {
             map = SpreadHeight(map, location, false);
         }
 
+        map = createTiles(map);
 
         return map;
     }
@@ -101,5 +102,143 @@ class MapGenerator {
       ////////////////////////
      // tile map generator //
     ////////////////////////
-    
+    private static int[][] createTiles(int[][] heightMap)
+    {
+        //smooth height, do 3 times to make sure everything is good.
+        SmoothHeight(heightMap);
+        SmoothHeight(heightMap);
+        SmoothHeight(heightMap);
+
+        // create the empty map.
+        // and add the tiles in one sweep.
+        int[][] map = new int[heightMap.Length][];
+        for (int i = 0; i < map.Length; i++)
+        {
+            map[i] = new int[heightMap[0].Length];
+            for (int j = 0; j < map[i].Length; j++)
+            {
+                // at a tile
+                map = SetTile(map, heightMap, i, j);
+            }
+        }   
+        return map; 
+    }
+    private static int[][] SmoothHeight(int[][] map)
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            for (int j = 0; j < map[0].Length; j++)
+            {
+                if (map[i][j] == 0 && NearbyCheck(map, i, j) == 0)
+                {
+                    map[i][j] = 1;
+                } else if (map[i][j] == 1 && NearbyCheck(map, i, j) > 2)
+                {
+                    map[i][j] = 0;
+                }
+                else if (map[i][j] == 1 && 
+                (
+                    (i != 0 && i < map.Length - 1 
+                    && map[i + 1][j] == 0 && map[i - 1][j] == 0) 
+                    ||
+                    (j != 0 && j < map[0].Length - 1 
+                    && map[i][j + 1] == 0 && map[i][j - 1] == 0) 
+                ))
+                {
+                    map[i][j] = 0;
+                }
+            }
+        }
+        return map;
+    }
+    private static int[][] SetTile(int[][] map, int[][] heightMap, int i, int j)
+    {
+        //int nearbySeaCount = NearbyCheck(heightMap, i, j);
+        //return if it is the sea.
+        if (heightMap[i][j] == 0)
+        {
+            return map;
+        }
+        //check for higher land (can't be a beach).
+        if (heightMap[i][j] > 1) {
+            map[i][j] = 13;
+            return map;
+        }
+        // height == 1! check for beaches and set.
+        if (i != 0 && heightMap[i - 1][j] == 0)
+        { //water on top
+            if (j < heightMap[0].Length - 1 && heightMap[i][j + 1] == 0) 
+            { //water to the right, corner beach.
+                map[i][j] = 6;
+            } else if (j != 0 && heightMap[i][j - 1] == 0)
+            { //water to the left, corner beach.
+                map[i][j] = 7;
+            } else 
+            { //flat beach.
+                map[i][j] = 1;
+            }
+            return map;
+        } else if (i != heightMap.Length - 1 && heightMap[i + 1][j] == 0)
+        { //water on bottom
+            if (j < heightMap[0].Length - 1 && heightMap[i][j + 1] == 0) 
+            { //water to the right, corner beach.
+                map[i][j] = 5;
+            } else if (j != 0 && heightMap[i][j - 1] == 0)
+            { //water to the left, corner beach.
+                map[i][j] = 8;
+            } else 
+            { //flat beach.
+                map[i][j] = 3;
+            }
+            return map;
+        } else if (j != 0 && heightMap[i][j - 1] == 0)
+        { //water on left
+            map[i][j] = 2;
+            return map;
+        } else if (j < heightMap[0].Length - 1 && heightMap[i][j + 1] == 0)
+        { //water on right
+            map[i][j] = 4;
+            return map;
+        } else if (j != 0 && i != 0 && heightMap[i - 1][j - 1] == 0)
+        { //water on top left
+            map[i][j] = 12;
+            return map;
+        } else if (j < heightMap[0].Length - 1 && i != 0 && heightMap[i - 1][j + 1] == 0)
+        { //water on top right
+            map[i][j] = 11;
+            return map;
+        } else if (j != 0 && i < heightMap.Length - 1 && heightMap[i + 1][j - 1] == 0)
+        { //water on bottom left
+            map[i][j] = 9;
+            return map;
+        } else if (j < heightMap[0].Length - 1 && i < heightMap.Length - 1 && heightMap[i + 1][j + 1] == 0)
+        { //water on bottom right
+            map[i][j] = 10;
+            return map;
+        }
+
+        map[i][j] = 13;
+        return map;
+    }
+    private static int NearbyCheck(int[][] map, int i, int j)
+    {
+        int count = 0; // amount of sea tiles.
+        if (i != 0 && map[i - 1][j] == 0)
+        {
+            count++;
+        }
+        if (j != 0 && map[i][j - 1] == 0)
+        {
+            count++;
+        }
+        if (i < map.Length - 1 && map[i + 1][j] == 0) 
+        {
+            count++;
+        }
+        if (j < map[0].Length - 1 && map[i][j + 1] == 0) 
+        {
+            count++;
+        } 
+        return count;
+    }
 }
